bbc8600e130178011538b1e090436e0d53fa501e
kvn-media <kevin.subagio@gmail.com>
2024-03-17 22:30:25 +0700

    create sidebar

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./sidebar.css";
+import SidebarButton from './sidebarButton';
+import { MdFavorite } from "react-icons/md";
+import { FaGripfire, FaPlay } from "react-icons/fa";
+import { FaSignOutAlt } from "react-icons/fa";
+import { IoLibrary } from "react-icons/io5";
+import { MdSpaceDashboard } from "react-icons/md";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar-container">
+      <img src="https://i.pinimg.com/736x/4b/e8/b9/4be8b9842a4bfe0ff89b2a0d6d3a2cae.jpg" className="profile-img" alt="profile" />
+      <div>
+          <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard /> } />
+          <SidebarButton title="Trending" to="/trending" icon={<FaGripfire /> } />
+          <SidebarButton title="Player" to="/player" icon={<FaPlay /> } />
+          <SidebarButton title="Favorites" to="/favorites" icon={<MdFavorite /> } />
+          <SidebarButton title="Library" to="/" icon={<IoLibrary /> } />
+      </div>
+      <SidebarButton title="Sign Out" to="" icon={<FaSignOutAlt /> } />
+    </div>
+  );
+  
+  
+}
